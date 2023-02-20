851b861a4d7bfdb02546e51e31ee3f6bc8418a40
Leander <leander.peter@finn.auto>
2023-02-20 18:53:44 +0100

    [Report] Aurora reports

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config.InitialOptions = {
   collectCoverage: false,
   coverageDirectory: "coverage",
   coveragePathIgnorePatterns: ["/node_modules/", "/postgres/migrations/"],
-  coverageReporters: ["text-summary", "html", "cobertura"],
+  coverageReporters: ["text-summary", "html", "cobertura", "text"],
   // For supporting absoulute imports
   moduleNameMapper: {
     "~/(.*)$": "<rootDir>/$1",
--- stress-tests/Subscriptions-API/create-subscriptions-and-lifecycle/high-load/response-parser.js
+++ stress-tests/Subscriptions-API/create-subscriptions-and-lifecycle/high-load/response-parser.js
@@ -4,6 +4,24 @@ function printStatus(requestParams, response, context, ee, next) {
   return next();
 }
 
+function trapError(requestParams, context, ee, next) {
+  if (!requestParams.hooks) {
+    requestParams.hooks = {};
+  }
+  if (!requestParams.hooks.beforeError) {
+    requestParams.hooks.beforeError = [];
+  }
+  requestParams.hooks.beforeError.push((error) => {
+    if (error.name !== "HTTPError") {
+      console.error(requestParams.url, error.name);
+      console.log(requestParams);
+    }
+    return error;
+  });
+  next();
+}
+
 module.exports = {
   printStatus: printStatus,
+  trapError: trapError,
 };
